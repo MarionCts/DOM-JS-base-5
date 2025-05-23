8d02a533724823b565815369eb6341333301332f
Marion Cts <104251209+MarionCts@users.noreply.github.com>
2025-05-23 14:53:40 +0200

    Updating solution

---
+++ script.js
@@ -0,0 +1,22 @@
+const users = [
+{ name: "Alice", age: 25 },
+{ name: "Bob", age: 30 },
+{ name: "Charlie", age: 35 },
+];
+
+console.log(users.length);
+
+let userBase = [];
+let userList = document.querySelector("#userList");
+
+// for (i = 0; i<users.length; i++) {
+//     userBase[i] = document.createElement("li");
+//     userBase[i].textContent = (`Nom : ${users[i].name}, Âge : ${users[i].age}`);
+//     userList.append(userBase[i]);
+// }
+
+for (i of users) {
+    userBase[i] = document.createElement("li");
+    userBase[i].textContent = (`Nom : ${i.name}, Âge : ${i.age}`);
+    userList.append(userBase[i]);     
+}
